4260aa352894f005bbdb8af9ef86e1d8432fbf26
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2021-04-23 00:56:49 +0200

    implemented sensor reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,108 +1,69 @@
 #include <Arduino.h>
 
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
+#include <map>
 #include <Wire.h>
 
-Adafruit_MPU6050 mpu;
+#define MPU_ADDR 0x68
+#define POWER_MANAGEMENT_REGISTER 0x6B
+#define GYRO_CONFIG_REGISTER 0x1B
+#define ACCELEROMETER_CONFIG_REGISTER 0x1C
+
+#define GYRO_READING_REGISTER 0x43
+#define GYRO_REGISTER_SIZE 6
+
+#define X "x"
+#define Y "y"
+#define Z "z"
+
+using GyroData = std::map<String, int>;
+
+void beginMPU() {
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(POWER_MANAGEMENT_REGISTER);
+    Wire.write(0);
+    Wire.endTransmission();
+
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(GYRO_CONFIG_REGISTER);
+    Wire.write(0);
+    Wire.endTransmission();
+
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(ACCELEROMETER_CONFIG_REGISTER);
+    Wire.write(0);
+    Wire.endTransmission();
+}
+
+GyroData readGyro() {
+    GyroData gyroReadings;
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(GYRO_READING_REGISTER);
+    Wire.endTransmission();
+    Wire.requestFrom(MPU_ADDR, GYRO_REGISTER_SIZE);
+
+    while (Wire.available() < 6);
+    gyroReadings[X] = Wire.read() << 8 | Wire.read();
+    gyroReadings[Y] = Wire.read() << 8 | Wire.read();
+    gyroReadings[Z] = Wire.read() << 8 | Wire.read();
+    return gyroReadings;
+}
 
 void setup() {
     Serial.begin(115200);
+    Wire.begin(21, 22);
 
-    if (!mpu.begin()) {
-        Serial.println("couldn't find MPU");
-
-        while(true) {
-            delay(1000);
-        }
-    }
-    Serial.println("found MPU");
-
-    switch (mpu.getAccelerometerRange()) {
-        case MPU6050_RANGE_2_G:
-            Serial.println("+-2G");
-            break;
-        case MPU6050_RANGE_4_G:
-            Serial.println("+-4G");
-            break;
-        case MPU6050_RANGE_8_G:
-            Serial.println("+-8G");
-            break;
-        case MPU6050_RANGE_16_G:
-            Serial.println("+-16G");
-            break;
-    }
-
-    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-    Serial.print("Gyro range set to: ");
-    switch (mpu.getGyroRange()) {
-        case MPU6050_RANGE_250_DEG:
-            Serial.println("+- 250 deg/s");
-            break;
-        case MPU6050_RANGE_500_DEG:
-            Serial.println("+- 500 deg/s");
-            break;
-        case MPU6050_RANGE_1000_DEG:
-            Serial.println("+- 1000 deg/s");
-            break;
-        case MPU6050_RANGE_2000_DEG:
-            Serial.println("+- 2000 deg/s");
-            break;
-    }
-
-    mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
+    beginMPU();
 }
 
 void loop() {
-    /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-
-  /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
-
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
-
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");
-
-  Serial.println("");
-  delay(500);
+    GyroData gyroReadings = readGyro();
+    String output;
+    GyroData::iterator iterator = gyroReadings.begin();
+    while (iterator != gyroReadings.end()) {
+        output += iterator->first + ":" + iterator->second + " ";
+        iterator++;
+    }
+    output += "\n";
+    Serial.print(output);
+    delay(500);
 }
