52733c2ae0e3079679f0d66517a2f6472b530e9e
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2021-04-29 09:46:00 +0200

    Use ServoInput library for input

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,62 @@
 #include <map>
 #include <Wire.h>
 
-#include <RCReceiverReader.h>
+#include <ServoInput.h>
 #include <MPU6050.h>
 #include <PID.h>
 
-#define RUDDER_PIN 2
-#define RUDDER_MIN 1068
-#define RUDDER_MAX 1871
+#include <ESP32Servo.h>
 
-RCReceiverReader<RUDDER_PIN> rudder(RUDDER_MIN, RUDDER_MAX);
+#define RUDDER_INPUT_PIN 4
+#define RUDDER_MIN 1000 //1068
+#define RUDDER_MAX 2000 //1871
+#define RUDDER_OUTPUT_PIN 14
+#define RUDDER_DIRECTION 0
+
+int16_t MAX_YAW_RATE = 360;
+int16_t MAX_PITCH_RATE  =360;
+int16_t MAX_ROLL_RATE = 720;
+
+
+ServoInputPin<RUDDER_INPUT_PIN> rudderInput(RUDDER_MIN, RUDDER_MAX);
 MPU6050 mpu = MPU6050();
 PID pid;
+
+Servo rudderServo;
 
 void setup() {
     Serial.begin(115200);
 
     mpu.begin();
+
+    while (!ServoInput.available()) {  // wait for all signals to be ready
+		log_i("Waiting for servo signals...");
+		delay(500);
+	}
+
+    ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+	rudderServo.setPeriodHertz(50);
+	rudderServo.attach(RUDDER_OUTPUT_PIN, RUDDER_MIN, RUDDER_MAX);
 }
 
 void loop() {
-    // RotationData setpoint;
-    // for (int i = 0; i < 3; i++) {
-    //     setpoint[i] = 0;
-    // }
-    // RotationData gyroReadings = mpu.getRotation();
-    // RotationData output = pid.loop(setpoint, gyroReadings);
-    // log_d("Gyro\t\tx:%7.2f, y:%7.2f, z:%7.2f", gyroReadings.x, gyroReadings.y, gyroReadings.z);
-    // log_d("Setpoint\tx:%7.2f, y:%7.2f, z:%7.2f", setpoint.x, setpoint.y, setpoint.z);
-    // log_d("Output\t\tx:%7.2f, y:%7.2f, z:%7.2f\n", output.x, output.y, output.z);
-    int16_t rudderValue = rudder.getPosition();
-    float rudderAngle = rudder.getAngle();
+    RotationData setpoint;
+    float angle = rudderInput.getAngle();
+    log_d("Angle: %.2f", angle);
+    setpoint[RUDDER_DIRECTION] = map(angle, 0, 180, -MAX_YAW_RATE, MAX_YAW_RATE);
 
-    log_d("Rudder:\tposition:%5d, angle:%7.2f", rudderValue, rudderAngle);
-    delay(100);
+    RotationData gyroReadings = mpu.getRotation();
+    RotationData output = pid.loop(setpoint, gyroReadings);
+    output[RUDDER_DIRECTION] = map(output[RUDDER_DIRECTION], -MAX_YAW_RATE, MAX_YAW_RATE, 0, 180);
+
+    log_d("Gyro\t\tx:%7.2f, y:%7.2f, z:%7.2f", gyroReadings.x, gyroReadings.y, gyroReadings.z);
+    log_d("Setpoint\tx:%7.2f, y:%7.2f, z:%7.2f", setpoint.x, setpoint.y, setpoint.z);
+    log_d("Output\t\tx:%7.2f, y:%7.2f, z:%7.2f\n", output.x, output.y, output.z);
+
+    rudderServo.write(output[RUDDER_DIRECTION]);
+
+    delay(20);
 }
