b2be70fc396b0812a30b5b103497a40b79e80dbb
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2021-05-11 19:24:44 +0200

    use yaw, pitch, roll instead of x, y, z

--- lib/OutputCalculator/OutputCalculator.cpp
+++ lib/OutputCalculator/OutputCalculator.cpp
@@ -12,16 +12,16 @@ RotationData OutputCalculator::calculateOutput(RotationData servoInput) {
     if (!this->shouldCalculate) return servoInput;
 
     RotationData setpoint;
-    setpoint.x = map(servoInput.x, 0, 180, -this->maxRates.x, this->maxRates.x);
-    setpoint.y = map(servoInput.y, 0, 180, -this->maxRates.y, this->maxRates.y);
-    setpoint.z = map(servoInput.z, 0, 180, -this->maxRates.z, this->maxRates.z);
+    setpoint.yaw = map(servoInput.yaw, 0, 180, this->maxRates.yaw, -this->maxRates.yaw);
+    setpoint.pitch = map(servoInput.pitch, 0, 180, this->maxRates.pitch, -this->maxRates.pitch);
+    setpoint.roll = map(servoInput.roll, 0, 180, -this->maxRates.roll, this->maxRates.z);
 
 
     RotationData gyroReadings = rotationReader->getRotation();
     RotationData output = pid->loop(setpoint, gyroReadings);
-    output.x = map(output.x, -this->maxRates.x, this->maxRates.x, 0, 180);
-    output.y = map(output.y, -this->maxRates.y, this->maxRates.y, 0, 180);
-    output.z = map(output.z, -this->maxRates.z, this->maxRates.z, 0, 180);
+    output.yaw = map(output.yaw, -this->maxRates.yaw, this->maxRates.yaw, 0, 180);
+    output.pitch = map(output.pitch, -this->maxRates.pitch, this->maxRates.pitch, 0, 180);
+    output.roll = map(output.roll, -this->maxRates.roll, this->maxRates.roll, 0, 180);
 
     log_d("Gyro\t\tx:%7.2f, y:%7.2f, z:%7.2f", gyroReadings.x, gyroReadings.y, gyroReadings.z);
     log_d("Setpoint\tx:%7.2f, y:%7.2f, z:%7.2f", setpoint.x, setpoint.y, setpoint.z);
