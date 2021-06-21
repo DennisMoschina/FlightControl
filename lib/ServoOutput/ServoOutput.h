0103c1f46fdb76459c6199afea2ca7978fcc584c
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2021-06-21 22:30:15 +0200

    added documentation

--- lib/ServoOutput/ServoOutput.h
+++ lib/ServoOutput/ServoOutput.h
@@ -3,9 +3,22 @@
 
 #include <RotationRateOuput.h>
 
+/**
+ * A {@link RotationRateOutput} in form of a servo.
+ * @author Dennis Moschina
+ * @version 1.0
+ */
 class ServoOutput: public RotationRateOutput {
 public:
+    /**
+     * Trim the servo.
+     * @param middle the new middle of the servo
+     */
     virtual void setMiddle(int middle) = 0;
+    /**
+     * Set the new range of the servo.
+     * @param range the new range for the servo
+     */
     virtual void setRange(int range) = 0;
 };
 
