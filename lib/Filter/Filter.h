d470b527495317df623bca82f815d538f2771a22
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2021-07-28 14:52:49 +0200

    added combined filter

--- lib/Filter/Filter.h
+++ lib/Filter/Filter.h
@@ -14,4 +14,24 @@ public:
     virtual std::array<T, dimensions> addValue(T value[dimensions]) = 0;
 };
 
+template<typename T, int dimensions = 1>
+class CombinedFilter : public Filter<T, dimensions> {
+public:
+    CombinedFilter(Filter<T, dimensions>* firstFilter, Filter<T, dimensions>* secondFilter) {
+        this->firstFilter = firstFilter;
+        this->secondFilter = secondFilter;
+    }
+
+    std::array<T, dimensions> addValue(T value[dimensions]) {
+        std::array<T, dimensions> filteredValue = this->firstFilter->addValue(value);
+        T filteredArr[dimensions];
+        for (int i = 0; i < dimensions; i++) filteredArr[i] = filteredValue[i];
+        return this->secondFilter->addValue(filteredArr);
+    }
+
+private:
+    Filter<T, dimensions>* firstFilter;
+    Filter<T, dimensions>* secondFilter;
+};
+
 #endif
